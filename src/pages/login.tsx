73b78ca87acfddd634f5d61e53dc1748fe4a3941
abejide001 <abejidefemi1@gmail.com>
2020-11-22 02:22:59 +0100

    implement login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,24 +4,23 @@ import { Box, Button } from '@chakra-ui/core'
 import { Wrapper } from '../components/Wrapper'
 import { useRouter } from 'next/router'
 import { InputField } from '../components/InputField'
-import { useMutation } from 'urql'
-import { useRegisterMutation } from '../generated/graphql'
+import { useLoginMutation } from '../generated/graphql'
 import { toErrorMap } from '../utils/toErrorMap'
 
-interface registerProps {
+interface loginProps {
 
 }
 
-export const Register: React.FC<registerProps> = ({ }) => {
+export const Login: React.FC<loginProps> = ({ }) => {
     const router = useRouter()
-    const [, register] = useRegisterMutation()
+    const [, login] = useLoginMutation()
     return (
         <Wrapper>
             <Formik initialValues={{ username: "", password: "" }} onSubmit={async (values, { setErrors }) => {
-                const response = await register(values)
-                if (response.data?.register.errors) {
-                    setErrors(toErrorMap(response.data.register.errors))
-                } else if (response.data?.register.user) {
+                const response = await login(values)
+                if (response.data?.login.errors) {
+                    setErrors(toErrorMap(response.data.login.errors))
+                } else if (response.data?.login.user) {
                     router.push("/")
                 }
             }}>
@@ -31,7 +30,7 @@ export const Register: React.FC<registerProps> = ({ }) => {
                         <Box mt={4}>
                             <InputField name="password" placeholder="password" label="Password" type="password" required />
                         </Box>
-                        <Button type="submit" variantColor="teal" mt={4} isLoading={isSubmitting}>Register</Button>
+                        <Button type="submit" variantColor="teal" mt={4} isLoading={isSubmitting}>Login</Button>
                     </Form>
                 )
                 }
@@ -40,4 +39,4 @@ export const Register: React.FC<registerProps> = ({ }) => {
     )
 }
 
-export default Register
+export default Login
